e0cb0b2f256201fb783c76ddfdf966d8c7e62400
Luigi <posea.luigi99@gmail.com>
2023-09-21 11:41:57 +0300

    sesiune teme2

--- src/main/java/tests/SingleAuthorTest.java
+++ src/main/java/tests/SingleAuthorTest.java
@@ -5,6 +5,7 @@ import static org.testng.Assert.assertEquals;
 import java.time.Duration;
 
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import pages.*;
@@ -37,27 +38,5 @@ public class SingleAuthorTest extends BaseTest{
 
 	}
 	
-	@Test
-	public void checkingSingleAutorPercentages2(){
 	
-		//app.menu.navigateTo(app.menu.singleAuthorlink);
-		app.click(app.menu.singleAuthorlink);
-	
-	WebDriverWait wait= new WebDriverWait(driver, Duration.ofSeconds(10));
-	wait.until(ExpectedConditions.textToBePresentInElementLocated
-			(app.singleAuthor.dramaPercentage, "95%"));
-	
-	assertEquals(app.getElementText(singleAuthor.dramaPercentage), "95%");
-	
-	wait.until(ExpectedConditions.textToBePresentInElementLocated
-			(singleAuthor.biographyPercentage, "75%"));
-	
-	assertEquals(app.getElementText(singleAuthor.biographyPercentage), "75%");
-	
-	wait.until(ExpectedConditions.textToBePresentInElementLocated
-			(app.singleAuthor.cookbooksPercentage, "82%"));
-	assertEquals(app.getElementText(singleAuthor.cookbooksPercentage), "82%");
-		
-
-	}
 }
